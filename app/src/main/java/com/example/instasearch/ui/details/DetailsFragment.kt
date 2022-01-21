5fabccbbd510dc570f9edabe2d4c40dfd5a59de8
Chelsea Katsidzira <chelsea.katsidzira@bounteous.com>
2022-01-21 03:06:16 -0500

    Added screen design for details

--- app/src/main/java/com/example/instasearch/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/instasearch/ui/details/DetailsFragment.kt
@@ -1,8 +1,87 @@
 package com.example.instasearch.ui.details
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.instasearch.R
+import com.example.instasearch.data.UnsplashPhoto
+import com.example.instasearch.databinding.FragmentDetailsBinding
+import com.example.instasearch.ui.gallery.KEY_UNSPLASH_PHOTO
 
 class DetailsFragment: Fragment(R.layout.fragment_details) {
+    private lateinit var photo: UnsplashPhoto
+    private lateinit var binding: FragmentDetailsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        arguments?.getParcelable<UnsplashPhoto>(KEY_UNSPLASH_PHOTO)?.let {
+            photo = it
+        }
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentDetailsBinding.bind(view)
+
+        binding.apply {
+            Glide.with(this@DetailsFragment)
+                .load(photo.urls.regular)
+                .error(R.drawable.ic_error)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        detailProgressBar.isVisible = true
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        detailProgressBar.isVisible = false
+                        detailCreatorTv.isVisible = true
+                        detailDescriptionTv.isVisible = photo.description != null
+                        return false
+                    }
+                })
+                .into(detailIv)
+
+            detailDescriptionTv.text = photo.description
+
+            val uri = Uri.parse(photo.user.attributionUrl)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+
+            detailCreatorTv.apply {
+                text = resources.getString(R.string.photo_by, photo.user.name)
+                setOnClickListener {
+                    context.startActivity(intent)
+                }
+                paint.isUnderlineText = true
+            }
+        }
+    }
 
 }
