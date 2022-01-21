45b0907706ad1b43c5f1d02be3124390837b58f1
Chelsea Katsidzira <chelsea.katsidzira@bounteous.com>
2022-01-21 01:33:55 -0500

    Initialize progress bar when results don't load

--- app/src/main/java/com/example/instasearch/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/instasearch/ui/gallery/GalleryFragment.kt
@@ -2,9 +2,14 @@ package com.example.instasearch.ui.gallery
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.paging.LoadState
 import androidx.paging.LoadStateAdapter
 import com.example.instasearch.R
 import com.example.instasearch.databinding.FragmentGalleryBinding
@@ -26,15 +31,63 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
         binding.apply {
             galleryRecyclerview.setHasFixedSize(true)
+            galleryRecyclerview.itemAnimator = null
             galleryRecyclerview.adapter = adapter.withLoadStateHeaderAndFooter(
                 header = PhotoLoadStateAdapter { adapter.retry() },
                 footer = PhotoLoadStateAdapter { adapter.retry() }
             )
+            retryButton.setOnClickListener {
+                adapter.retry()
+            }
         }
 
         viewModel.photos.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
+
+        adapter.addLoadStateListener { loadState ->
+            binding.apply {
+                galleryProgressBar.isVisible = loadState.source.refresh is LoadState.Loading
+                galleryRecyclerview.isVisible = loadState.source.refresh is LoadState.NotLoading
+                retryButton.isVisible = loadState.source.refresh is LoadState.Error
+                errorTv.isVisible = loadState.source.refresh is LoadState.Error
+
+                if (loadState.source.refresh is LoadState.NotLoading &&
+                        loadState.append.endOfPaginationReached &&
+                        adapter.itemCount < 1) {
+                    galleryRecyclerview.isVisible = false
+                    noResultsTv.isVisible = true
+                } else {
+                    noResultsTv.isVisible = false
+                }
+            }
+        }
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.menu_gallery, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    binding.galleryRecyclerview.scrollToPosition(0)
+                    viewModel.searchPhotos(query)
+                    searchView.clearFocus()
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
     }
 
 }
